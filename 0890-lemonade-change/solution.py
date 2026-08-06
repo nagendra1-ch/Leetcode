d397db5c68aa31cac2161a2ab1fe077768d246dd
nagendra1-ch <chnagendra592@gmail.com>
2026-08-06 09:26:15 +0000

    Sync LeetCode submission Runtime - 3 ms (73.35%), Memory - 23.5 MB (15.40%)

---
+++ 0890-lemonade-change/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        fives=0
+        tens=0
+        twenty=0
+
+        for bill in bills:
+            if bill==5:
+                fives+=1
+            elif bill==10:
+                tens+=1
+                if fives>0:
+                    fives-=1
+                else:
+                    return False
+            elif bill==20:
+                twenty+=1
+                if fives>=1 and tens>=1:
+                    fives-=1
+                    tens-=1
+                elif fives>=3:
+                    fives-=3
+                else:
+                    return False
+        return True
