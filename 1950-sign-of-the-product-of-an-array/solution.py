25b0c862e756f68b20cf7e9a8e7ac63cbf2b3f30
nagendra1-ch <chnagendra592@gmail.com>
2026-08-13 09:18:06 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (34.13%)

--- 1950-sign-of-the-product-of-an-array/solution.py
+++ 1950-sign-of-the-product-of-an-array/solution.py
@@ -1,12 +1,9 @@
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
-        ans=1
+        sign=1
         for i in nums:
-            ans*=i
-        if ans<0:
-            return -1
-        elif ans>0:
-            return 1
-        else:
-            return 0
-        
+            if i<0:
+                sign*=-1
+            elif i==0:
+                return 0
+        return sign
